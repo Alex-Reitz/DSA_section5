9209f098c1ba3df53c36dda6d25ced211b65e377
Alex Reitz <alex.j.reitz@gmail.com>
2021-08-09 13:05:13 -0400

    binary search trees

--- 47.9 dsa-bsts-demo/bst.js
+++ 47.9 dsa-bsts-demo/bst.js
@@ -1,5 +1,5 @@
 class BinarySearchNode {
-  constructor(val, left=null, right=null) {
+  constructor(val, left = null, right = null) {
     this.val = val;
     this.left = left;
     this.right = right;
@@ -11,30 +11,25 @@ class BinarySearchNode {
     let current = this;
 
     while (current) {
-      if (current.val === sought) 
-        return current;
+      if (current.val === sought) return current;
 
-      current = sought < current.val
-                ? current.left
-                : current.right;
+      current = sought < current.val ? current.left : current.right;
     }
   }
   // end
 }
 // end
 
+let apple = new BinarySearchNode("apple");
+let ghost = new BinarySearchNode("ghost");
+let fence = new BinarySearchNode("fence", apple, ghost);
+let just = new BinarySearchNode("just");
+let jackal = new BinarySearchNode("jackal", fence, just);
+let zebra = new BinarySearchNode("zebra");
+let pencil = new BinarySearchNode("pencil", null, zebra);
+let mystic = new BinarySearchNode("mystic");
+let nerd = new BinarySearchNode("nerd", mystic, pencil);
+let money = new BinarySearchNode("money", jackal, nerd);
 
-
-let apple = new BinarySearchNode("apple")
-let ghost = new BinarySearchNode("ghost")
-let fence = new BinarySearchNode("fence", apple, ghost)
-let just = new BinarySearchNode("just")
-let jackal = new BinarySearchNode("jackal", fence, just)
-let zebra = new BinarySearchNode("zebra")
-let pencil = new BinarySearchNode("pencil", null, zebra)
-let mystic = new BinarySearchNode("mystic")
-let nerd = new BinarySearchNode("nerd", mystic, pencil)
-let money = new BinarySearchNode("money", jackal, nerd)
-
-console.log("nerd = ", money.find("nerd"))
-console.log("banana = ", money.find("banana"))
+console.log("nerd = ", money.find("nerd"));
+console.log("banana = ", money.find("banana"));
--- 47.9 dsa-bsts/binary-search-tree.js
+++ 47.9 dsa-bsts/binary-search-tree.js
@@ -13,82 +13,198 @@ class BinarySearchTree {
 
   /** insert(val): insert a new node into the BST with value val.
    * Returns the tree. Uses iteration. */
-
+  //Tree is empty, root is the first node we insert, then compare next node to insert
+  //If larger insert as right child, if smaller than root, insert as left child
+  //If child nodes already exist, advance to the next child node and insert where appropriate
   insert(val) {
-
+    if (this.root === null) {
+      this.root = new Node(val);
+      return this;
+    }
+    var current = this.root;
+    while (true) {
+      if (val < current.val) {
+        if (current.left === null) {
+          current.left = new Node(val);
+          return this;
+        } else {
+          current = current.left;
+        }
+      } else if (val > current.val) {
+        if (current.right === null) {
+          current.right = new Node(val);
+          return this;
+        } else {
+          current = current.right;
+        }
+      }
+    }
   }
 
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
   insertRecursively(val) {
-
+    //Check if we have a root node
+    if (this.val === val) {
+      throw new Error("Data already exists within tree");
+    } else if (this.val > val) {
+      if (this.left) {
+        this.left.insertRecursively(val);
+      } else {
+        this.left = new Node(val);
+      }
+    } else {
+      if (this.right) {
+        this.right.insertRecursively(val);
+      } else {
+        this.right = new Node(data);
+      }
+    }
   }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
-
+  //start at root node
+  //search subtree according to val at currentNode
   find(val) {
+    let currentNode = this.root;
+    let found = false;
 
+    if (val === currentNode.val) return currentNode;
+
+    while (currentNode && !found) {
+      if (val < currentNode.val) {
+        currentNode = currentNode.left;
+      } else if (val > currentNode.val) {
+        currentNode = currentNode.right;
+      } else {
+        found = true;
+      }
+    }
+
+    if (!found) return undefined;
+    return currentNode;
   }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
-
-  findRecursively(val) {
-
+  findRecursively(val, current = this.root) {
+    if (this.root === null) return undefined;
+    //If value at currentNode is larger than val we are looking for
+    //recurse left
+    //if value at currentNode is smaller than val we are looking for
+    //recurse right
+    if (val < current.val) {
+      if (current.left === null) return undefined;
+      return this.findRecursively(val, current.left);
+    } else if (val > current.val) {
+      if (current.right === null) return undefined;
+      return this.findRecursively(val, current.right);
+    }
+    return current;
   }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
-
+  //Print node
+  //traverse left subtree
+  //traverse right subtree
+  //PLR
   dfsPreOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      data.push(node.val);
+      node.left && traverse(node.left);
+      node.right && traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
-
+  //traverse the left subtree
+  //print node
+  //then traverse right subtree
+  //LPR
   dfsInOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      node.left && traverse(node.left);
+      data.push(node.val);
+      node.right && traverse(node.right);
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
-
+  //traverse left
+  //traverse right
+  //print node
+  //LRP
   dfsPostOrder() {
+    let data = [];
+    let current = this.root;
 
+    function traverse(node) {
+      node.left && traverse(node.left);
+      node.right && traverse(node.right);
+      data.push(node.val);
+    }
+
+    traverse(current);
+    return data;
   }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
   bfs() {
+    let node = this.root;
+    let queue = [];
+    let data = [];
 
+    queue.push(node);
+
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val);
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return data;
   }
 
   /** Further Study!
    * remove(val): Removes a node in the BST with the value val.
    * Returns the removed node. */
 
-  remove(val) {
-
-  }
+  remove(val) {}
 
   /** Further Study!
    * isBalanced(): Returns true if the BST is balanced, false otherwise. */
 
-  isBalanced() {
-
-  }
+  isBalanced() {}
 
   /** Further Study!
    * findSecondHighest(): Find the second highest value in the BST, if it exists.
    * Otherwise return undefined. */
 
-  findSecondHighest() {
-    
-  }
+  findSecondHighest() {}
 }
 
 module.exports = BinarySearchTree;
